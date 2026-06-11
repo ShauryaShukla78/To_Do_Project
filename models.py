b97a9a957251b122b458cca0dae6e7a0cd5e2ed7
ShauryaShukla78 <shauryash5890@gmail.com>
2026-06-11 15:38:55 +0530

    Add modified file

--- models.py
+++ models.py
@@ -1,15 +1,17 @@
-import uuid
+import uuid # for giving unique id
 from datetime import date
 
 class Task:
+    # constructor when new task object is formed
     def __init__(self, name, priority, due_date, status="Pending", created=None, task_id=None):
-        self.id = task_id or str(uuid.uuid4())
+        self.id = task_id or str(uuid.uuid4()) # for generating id after or 
         self.name = name
         self.priority = priority
         self.due_date = due_date
         self.status = status
         self.created = created or str(date.today())
 
+# converting task object to dictionary , helps for json
     def to_dict(self):
         return {
             "id": self.id,
@@ -19,7 +21,7 @@ class Task:
             "status": self.status,
             "created": self.created
         }
-
+# to convert dict back into task obj
     @staticmethod
     def from_dict(data):
         return Task(
